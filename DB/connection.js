943c68a9d63f4ec33f8a43550e7548dfdf944bc7
Ali Ahmed Samoo <aliahmed.samoo.1@gmail.com>
2026-05-04 16:48:59 +0500

    safd

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const uri = "mongodb://localhost:27017/";
+const uri = "mongodb://admin:admin@ac-u5bdorw-shard-00-00.ddpi0ha.mongodb.net:27017,ac-u5bdorw-shard-00-01.ddpi0ha.mongodb.net:27017,ac-u5bdorw-shard-00-02.ddpi0ha.mongodb.net:27017/?ssl=true&replicaSet=atlas-j1i9ns-shard-0&authSource=admin&appName=FFECO";
 mongoose.connect(uri).then(() => {
     console.log("Connection is successful with DataBase");
 }).catch((err) => {
